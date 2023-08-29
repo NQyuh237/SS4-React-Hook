533e27b9691e5aadf77a33776c16ab14b251cbce
nguyen huy <quanghuy2371998@gmail.com>
2023-08-29 17:01:54 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+// import Th8 from "./components/th/Th8";
+import Bt1 from "./components/bt/Bt1";
+// import Th6 from './components/th/Th6'
+// import Th7 from './components/th/Th7'
+// import Th4 from './components/th/Th4'
+// import Th5 from './components/th/Th5'
+// import Th2 from './components/th/th2'
+// import Th3 from './components/th/Th3'
+// import Th4 from './components/th/Th4'
 // import UseReducer from './components/useReducer/UseReducer'
-import Todolist from './components/todolist/Todolist'
+// import Todolist from './components/todolist/Todolist'
+// import Th1 from './components/th/Th1'
 // import UseState_Comp from './components/useState/UseState_Comp'
 // import UseContex_Comp from './components/useContext/UseContex_Comp'
 // import UseEffect from './components/useEffect/UseEffect'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,9 +27,18 @@ function App() {
       {/* <UseContex_Comp/> */}
       {/* <UseEffect/> */}
       {/* <UseReducer/> */}
-      <Todolist/>
+      {/* <Todolist/> */}
+      {/* <Th1/>*/}
+      {/* <Th2/> */}
+      {/* <Th3/> */}
+      {/* <Th4/> */}
+      {/* <Th5/> */}
+      {/* <Th6 /> */}
+      {/* <Th7/> */}
+      {/* <Th8/> */}
+      <Bt1/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/th/Th7.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function Th7() {
+  const [celsius, setCelsius] = useState("");
+  const [fahrenheit, setFahrenheit] = useState("");
+
+  const handleCelsiusChange = (event) => {
+    setCelsius(event.target.value);
+    setFahrenheit((event.target.value * 9) / 5 + 32);
+  };
+
+  const handleFahrenheitChange = (event) => {
+    setFahrenheit(event.target.value);
+    setCelsius(((event.target.value - 32) * 5) / 9);
+  };
+
+  return (
+    <div>
+      <div>
+        <label>Celsius:</label>
+        <input type="number" value={celsius} onChange={handleCelsiusChange} />
+      </div>
+      <div>
+        <label>Fahrenheit:</label>
+        <input
+          type="number"
+          value={fahrenheit}
+          onChange={handleFahrenheitChange}
+        />
+      </div>
+    </div>
+  );
+}
